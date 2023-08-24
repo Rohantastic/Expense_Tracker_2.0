f66f5f06d617c36090630f90011228bb93d164c7
rohantastic <rohantastic@outlook.com>
2023-08-24 14:13:11 +0530

    added helmet and morgan

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 const UserRoute = require('./routes/User');
 const ExpenseRoute = require('./routes/expense');
 const PurchaseRoute = require('./routes/purchase');
@@ -10,18 +14,23 @@ const User = require('./models/User');
 const Expense = require('./models/expense');
 const Order = require('./models/Orders');
 const PasswordRequest = require('./models/ForgotPasswordRequests');
+const path = require('path');
 
 app.use(express.static('views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'});//creating a log files to view all console logs on a log file
 
 //routes
 app.use('/user', UserRoute);
 app.use('/expense', ExpenseRoute);
 app.use('/purchase',PurchaseRoute);
 app.use('/premium',PremiumRoute);
+app.use(helmet()); //gives security to headers
+app.use(compression()); //compresses the files over network
+app.use(morgan('combined',{ stream: accessLogStream})); //morgan helps log on terminal or file, than that of console(which makes systen laggy)
 
 
 //database relations
